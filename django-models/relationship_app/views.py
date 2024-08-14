2ef57704da68c05a9b401881d1eb6c71e3cf9c42
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-08-14 18:30:06 +0000

    add new file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
 
 def list_books(request):
     books = Book.objects.all()
@@ -12,3 +14,12 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
