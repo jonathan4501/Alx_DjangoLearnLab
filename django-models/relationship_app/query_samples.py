de4cbad6060256421fdb344be933ff2d89e2cc41
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-08-14 16:08:20 +0000

    add new file

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -9,7 +9,7 @@ for book in books_by_author:
     print(book.title)
 
 # List all books in a library
-library = Library.objects.get(name='New York Public Library')
+library = Library.objects.get(name='library_name')
 books_in_library = library.books.all()
 print("\nBooks in New York Public Library:")
 for book in books_in_library:
