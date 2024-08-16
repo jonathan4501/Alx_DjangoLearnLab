bc6bd0191cd88c90f8b8499ff0d25659de844c28
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-08-16 11:01:53 +0000

    add new file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
 from django.contrib.auth import login
-
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books(request):
     books = Book.objects.all()
@@ -26,3 +26,25 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+def member_view(request):
+    return render(request, 'member_view.html')
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+admin_view = user_passes_test(is_admin)(admin_view)
+librarian_view = user_passes_test(is_librarian)(librarian_view)
+member_view = user_passes_test(is_member)(member_view)
