d73d985f768e660b9a66ed66764057cfcca1d7db
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-08-16 11:07:52 +0000

    add new file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -27,24 +27,14 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
+@user_passes_test(lambda u: u.userprofile.role == 'Admin')
 def admin_view(request):
     return render(request, 'admin_view.html')
 
+@user_passes_test(lambda u: u.userprofile.role == 'Librarian')
 def librarian_view(request):
     return render(request, 'librarian_view.html')
 
+@user_passes_test(lambda u: u.userprofile.role == 'Member')
 def member_view(request):
     return render(request, 'member_view.html')
-
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
-
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
-
-admin_view = user_passes_test(is_admin)(admin_view)
-librarian_view = user_passes_test(is_librarian)(librarian_view)
-member_view = user_passes_test(is_member)(member_view)
