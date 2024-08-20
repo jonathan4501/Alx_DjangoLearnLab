f62a4af9179f9682c10522381914332fa89ed5e0
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-08-20 08:44:49 +0000

    add new file

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,14 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User, AbstractUser, BaseUserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos', null=True, blank=True)
-
-class UserManager(models.Manager):
+class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password, date_of_birth, profile_photo):
         if not email:
             raise ValueError('Users must have an email address')
@@ -35,7 +31,11 @@ class UserManager(models.Manager):
         user.save(using=self._db)
         return user
     
-    
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos', null=True, blank=True)
+    objects = CustomUserManager()
+ 
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
