6f4e7b80b601ac26a07cf5842efcd485c7c06a42
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-08-20 09:28:01 +0000

    add new file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from django.db.models import Q
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 
 def book_list(request):
     search = request.GET.get('search', '')
@@ -12,12 +12,12 @@ def book_list(request):
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookForm()
+        form = ExampleForm()
     return render(request, 'create_book.html', {'form': form})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -32,12 +32,12 @@ def edit_book(request, pk):
     except Book.DoesNotExist:
         return render(request, '404.html')
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
+        form = ExampleForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookForm(instance=book)
+        form = ExampleForm(instance=book)
     return render(request, 'edit_book.html', {'form': form, 'book': book})
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
