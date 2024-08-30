8c7eb4ee2a8b77d44732cbed7862ceaf35885263
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-08-30 15:19:07 +0000

    add new file

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
     path('books/', views.BookList.as_view()),
+    path('api/', include('api.urls')),
 ]
