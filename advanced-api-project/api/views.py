28b84efea8d243b3d9a67121493ce8b635f734ac
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-09-05 10:55:33 +0000

    add new file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import filters
 from .models import Book
 from .seriealizers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class BookListView(generics.ListAPIView):
@@ -11,6 +13,10 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
 
 class BookDetailView(generics.RetrieveAPIView):
     """
@@ -49,3 +55,19 @@ class BookDeleteView(generics.DestroyAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
+"""
+BookListView:
+This view provides a list of books and allows users to filter, search, and order the results.
+
+Filtering:
+You can filter books by title, author, and publication year by adding query parameters to the URL. For example:
+http://localhost:8000/api/books/?title=book_title
+
+Searching:
+You can search books by title and author by adding a search query parameter to the URL. For example:
+http://localhost:8000/api/books/?search=book_author
+
+Ordering:
+You can order books by title and publication year by adding an ordering query parameter to the URL. For example:
+http://localhost:8000/api/books/?ordering=publication_year
+"""
