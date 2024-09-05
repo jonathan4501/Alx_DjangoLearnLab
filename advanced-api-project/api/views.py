d0fb30fb87e07d5c8182458e3a016c040ef7af6c
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-09-05 11:01:57 +0000

    add new file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -14,7 +14,7 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title', 'author', 'publication_year']
     search_fields = ['title', 'author']
     ordering_fields = ['title', 'publication_year']
