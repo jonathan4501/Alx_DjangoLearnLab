400cfcef2706eb703ee141ee9d9de5f1f39de2d3
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-09-05 10:28:22 +0000

    add new file

--- advanced-api-project/advanced_api_project/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Author(models.Model):
-    name = models.CharField(max_length=255)
-
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
