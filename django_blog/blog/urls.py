1016fd4fc5c8aa695824525f8d2e5f5de7f952b3
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-09-10 10:20:32 +0000

    add new file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
-    path('post/<int:post_id>/comments/', views.CommentListView.as_view(), name='comment_list'),
-    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
-    path('post/<int:post_id>/comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_update'),
-    path('post/<int:post_id>/comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+    path('post/<int:post_id>/comment/', views.CommentListView.as_view(), name='comment_list'),
+    path('post/<int:post_id>/comment/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('post/<int:post_id>/comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('post/<int:post_id>/comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
