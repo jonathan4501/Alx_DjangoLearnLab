22275a43e07f2fa5f305577bce21978d36fafe0a
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-09-17 17:24:17 +0000

    add new file

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
@@ -11,3 +22,7 @@ class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
         fields = ['key']
+
+    def create(self, validated_data):
+        token = Token.objects.create(user=validated_data['user'])
+        return token
