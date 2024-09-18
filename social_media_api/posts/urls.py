48f717e4aa0dc357b0352fe5e4efbfdef97e7b83
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-09-18 15:02:49 +0000

    add new file

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import PostViewSet, CommentViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
