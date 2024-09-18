3b322c8d233bfc75eccf8f03817fd6e6c30b3b3b
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-09-18 14:40:26 +0000

    add new file

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    new_field = serializers.CharField()
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
