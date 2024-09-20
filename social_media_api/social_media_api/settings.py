82417aab41bbcc6f5bdb19158a082b19ee07f054
Jonathan Edem Koffi Ameko <112769062+jonathan4501@users.noreply.github.com>
2024-09-20 12:11:44 +0000

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_media_api_db',
+        'USER': 'social_media_api_user',
+        'PASSWORD': 'Redeemer1221!@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
